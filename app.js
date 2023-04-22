491b4ede07196cd9e94ed0c8d07da26420f477a9
ARPAN KUMAR MONDAL <arpankumarmondal96@gmail.com>
2023-04-22 23:33:13 +0530

    quiz app

--- app.js
+++ app.js
@@ -1,77 +1,89 @@
-const questions= [{
-    'que':'Which of the following is a markup language?',
-    'a':'HTML',
-    'b':'CSS',
-    'c':'JavaScript',
-    'd':'PHP',
-    'correct':'a'
-
+const questions = [{
+    'que': 'which of the following is a markup language?',
+    'a': 'HTML',
+    'b': 'CSS',
+    'c': 'JavaScript',
+    'd': 'PHP',
+    'correct': 'a',
 },
 {
-    'que':'What year was JavaScript launched?',
-    'a':'1996',
-    'b':'1995',
-    'c':'1994',
-    'd':'none of the above',
-    'correct':'b'
-
-
+    'que': "Which of the following is a client site language?",
+    'a': "Java",
+    'b': "C",
+    'c': "Python",
+    'd': "JavaScript",
+    'correct': 'd',
 },
 {
-    'que':'What does CSS Stands for?',
-    'a':'Hypertext Markup Language',
-    'b':'Cascading Style Sheet',
-    'c':'Jason Object Notation',
-    'd':'Helicopters Terminals',
-    'correct':'a'
-
+    'que': "What year was JavaScript launched?",
+    'a': "1996",
+    'b': "1995",
+    'c': "1994",
+    'd': "none of the above",
+    'correct': 'b',
 }
-]
+];
+
 let index = 0;
 let total = questions.length;
-let right=0,
-    wrong=0;
-const quesBox = document.getElementById("quesBox")
+let right = 0,
+wrong = 0;
+const quesbox = document.getElementById("quesbox")
 const optionInputs = document.querySelectorAll('.options')
-const loadQuestion= ()=>{
-    const data = questions[index]
-    quesBox.innerText = `${index+1}) ${data.que}`;
-    optionInputs[0].nextElementSibling.innerText = data.a;
-    optionInputs[1].nextElementSibling.innerText = data.b;
-    optionInputs[2].nextElementSibling.innerText = data.c;
-    optionInputs[3].nextElementSibling.innerText = data.d;
-
-
-
+const loadquestion = () => {
+if (index === total) {
+    return endQuiz();
 }
+reset();
+const data = questions[index]
+console.log(data);
+quesbox.innerText = ` ${index+1}) ${data.que}`;
+optionInputs[0].nextElementSibling.innerText = data.a;
+optionInputs[1].nextElementSibling.innerText = data.b;
+optionInputs[2].nextElementSibling.innerText = data.c;
+optionInputs[3].nextElementSibling.innerText = data.d;
+}
+
 const submitQuiz = () => {
-    const data = questions[index]
-    const ans = getAnswer()
-    if(ans== data.correct){
-        right++;
-
-    }else{
-        wrong++;
-
-    }
+const data = questions[index];
+const ans = getAnswer()
+if (ans == data.correct) {
+    right++;
+} else {
+    wrong++;
+}
+index++;
+loadquestion();
+return;
 }
 
 const getAnswer = () => {
-    optionInputs.forEach {
-
-    
-        (input) =>{
-            if(input.checked){
-                return input.value;
-                
-            } 
-
+let answer;
+optionInputs.forEach(
+    (input) => {
+        if (input.checked) {
+            answer = input.value;
         }
     }
-
+)
+return answer;
 }
 
+const reset = () => {
+optionInputs.forEach(
+    (input) => {
+        input.checked = false;
+    }
+)
+}
 
-// initial call
+const endQuiz = () => {
+document.getElementById("box").innerHTML = `
+<div style="text-align:center">
+    <h2> Thank you for playing the Quiz </h2>
+    <h2> ${right} / ${total} are correct </h2>
+    </div>
+`
+}
 
-loadQuestion();
+loadquestion();
